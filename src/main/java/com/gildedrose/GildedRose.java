58878f9e40e28425274f97ca502ebdc17ec33b64
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-09-09 22:37:25 +0200

    Refactoring part 1 + added implementation for 'Conjured Mana Cake'

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,54 +9,73 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
+            if (item.name.equals("Aged Brie")) {
+                updateAgedBrie(item);
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                updateBackstagePasses(item);
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                updateSulfuras(item);
+            } else if (item.name.equals("Conjured Mana Cake")) {
+                updateConjuredManaCake(item);
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
+                updateStandardItem(item);
             }
+        }
+    }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
-            }
+    private void updateStandardItem(Item item) {
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+            item.quality = item.quality - 2;
+        } else if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+        if (item.quality < 0) {
+            item.quality = 0;
+        }
+    }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+    private void updateConjuredManaCake(Item item) {
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+            item.quality = item.quality - 4;
+        } else if (item.quality > 0) {
+            item.quality = item.quality - 2;
+        }
+        if (item.quality > 50) {
+            item.quality = 50;
+        }
+    }
+
+    private void updateSulfuras(Item item) {
+        item.quality = 80;
+    }
+
+    private void updateBackstagePasses(Item item) {
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+            item.quality = 0;
+        } else if (item.sellIn < 5) {
+            item.quality = item.quality + 3;
+        } else if (item.sellIn < 10) {
+            item.quality = item.quality + 2;
+        } else {
+            item.quality = item.quality + 1;
+        }
+        if (item.quality > 50) {
+            item.quality = 50;
+        }
+    }
+
+    private void updateAgedBrie(Item item) {
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+            item.quality = item.quality + 2;
+        } else {
+            item.quality = item.quality + 1;
+        }
+        if (item.quality > 50) {
+            item.quality = 50;
         }
     }
 }
